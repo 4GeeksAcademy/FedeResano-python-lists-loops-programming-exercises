6a1b6634b03d40fc26efaff486205fcdcead1cd3
Federico Resano <federicoresano1@gmail.com>
2024-01-27 13:02:57 -0300

    commit

--- exercises/06.1-Everything_is_awesome/app.py
+++ exercises/06.1-Everything_is_awesome/app.py
@@ -3,8 +3,10 @@ my_list = [1, 0, 0, 0, 1, 0, 0, 0, 1, 1]
 def my_function(numbers):
     new_list = []
     for i in numbers:
-        # The magic happens here
-        
+        if numbers[i] == 1:
+            new_list.append(1)
+        elif numbers[i] == 0:
+            new_list.append("Yahoo")
         
     return new_list
     
