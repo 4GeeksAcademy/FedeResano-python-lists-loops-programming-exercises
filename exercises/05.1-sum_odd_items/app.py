a2bb991ee6cf28f057144e866aabfd543a429fcb
Federico Resano <federicoresano1@gmail.com>
2024-01-27 12:56:20 -0300

    commit

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,12 @@
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
-# Your code here
+def sum_odds():
+    total = 0
+    for i in range(0, len(my_list)):
+        if my_list[i] % 2 != 0:
+            total += my_list[i]
+
+    return total
+
+print(sum_odds())
+
