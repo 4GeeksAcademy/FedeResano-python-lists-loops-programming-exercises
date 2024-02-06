01756dfd8dcc59984cd5af9b64b3955d19a72c3b
Federico Resano <federicoresano1@gmail.com>
2024-02-05 21:00:04 -0300

    commit

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,1 +1,12 @@
-# Your code here
+def matrix_builder(num):
+    matrix = []
+
+    for i in range(num):
+        row = []
+        for j in range(num):
+            row.append(1)
+        matrix.append(row)
+
+    return matrix
+
+print(matrix_builder(3))
--- exercises/15.2-Parking_lot_check/app.py
+++ exercises/15.2-Parking_lot_check/app.py
@@ -4,4 +4,17 @@ parking_state = [
   [1,1,2]
 ]
 
-# Your code here
+def get_parking_lot(matrix):
+    parking = {"total_spots": 0, "available_spots": 0, "occupied_spots": 0}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                parking["occupied_spots"] += 1
+                parking["total_spots"] += 1
+            elif matrix[i][j] == 2:
+                parking["available_spots"] += 1
+                parking["total_spots"] += 1
+
+    return parking
+
+get_parking_lot(parking_state)
